5d14d5e018cfd35feab8c03667c82dfdce428a1d
Moina <moina.comoros@yahoo.fr>
2022-04-26 15:55:18 +0400

    creation edit.blade et route ajout bouton modif

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,36 +1,39 @@
 @extends('layouts.front')
 @section('title', 'La liste des posts de mon super blog')
 @section('content')
-<h1>Les films</h1>
-<p>Tous les articles sont ici, profitez-en ;)</p>
-@foreach($posts as $post)
+    <h1>Les films</h1>
+    <p>Tous les articles sont ici, profitez-en ;)</p>
+    @foreach ($posts as $post)
+        <table class="tablepost">
+            <tbody>
+                <tr>
+                    <td>
+                        <h3>{{ $post->title }}</h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>{{ Str::limit($post->content, 10) }}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h5><a href="{{ route('posts.show', $post) }}">Lire l'article</a></h5>
+                    </td>
+                    <!-- Fait maison  -->
 
-<table class="tablepost">
-    <tbody>
-        <tr>
-            <td>
-                <h3>{{ $post->title }}</h3>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <p>{{ Str::limit($post->content, 10) }}</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <h5><a href="{{ route('posts.show', $post) }}">Lire l'article</a></h5>
-            </td>
-            <!-- Fais maison  -->
-            @csrf
-            @method('DELETE')
-            <td><a href="{{ route('posts.destroy', $post) }}" class="text-red-600 hover:text-red-900">EFFACER</a></td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-    </tbody>
-    </tr>
-</table>
-@endforeach
+                    <td><a href="{{ route('posts.edit', $post) }}" class="text-red-600 hover:text-red-900">MODIFIER</a>
+                    </td>
+                    @csrf
+                    @method('DELETE')
+                    <td><a href="{{ route('posts.destroy', $post) }}" class="text-red-600 hover:text-red-900">EFFACER</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+            </tr>
+        </table>
+    @endforeach
 @endsection
