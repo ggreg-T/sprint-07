c00165c9f1b972de346c6136d80bd0a3bae3431d
Moina <moina.comoros@yahoo.fr>
2022-04-27 09:32:15 +0400

    Mise à jour du fichier edit.blade du post avec reprise des saisies

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,63 +1,56 @@
-<!-- @extends('layouts.front') -->
+ <!-- Nouveau code -->
 
-<!-- @section('content')
-    -->
+ <x-app-layout>
+     <x-slot name="header">
+         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+             Gestion des posts
+         </h2>
+     </x-slot>
 
-
-    <!-- Nouveau code -->
-
-    <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Gestion des posts
-            </h2>
-        </x-slot>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <h3 class="text-3xl mx-4 my-4">Modification d'un post</h3>
-                    @if (session('success'))
-                        <div class="text-xl text-green-400">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    <!-- ici un changement -->
-                    <form action="{{ route('posts.update', $post->id) }}" method="POST" class="mx-auto">
-                        @csrf
-                        @method('PATCH')
-                        <div class="px-4 py-5 bg-white sm:p-6">
-                            <div class="py-2">
-                                <label for="title" class="block text-sm font-medium text-gray-700">Titre</label>
-                                <input type="text" name="title" id="title"
-                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                    value="{{ old('title') }}">
-                                @error('title')
-                                    <span class="text-red-600">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="py-2">
-                                <label for="content" class="block text-sm font-medium text-gray-700">Contenu</label>
-                                <textarea id="content" name="content" rows="3"
-                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('content') }}</textarea>
-                                @error('content')
-                                    <span class="text-red-600">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="py-2">
-                                <input type="submit"
-                                    class="cursor-pointer inline-flex items-center w-1/4 py-4 border border-gray-400 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white justify-center"
-                                    value="Changer">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </x-app-layout>
+     <div class="py-12">
+         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                 <h3 class="text-3xl mx-4 my-4">Modification d'un post</h3>
+                 @if (session('success'))
+                     <div class="text-xl text-green-400">
+                         {{ session('success') }}
+                     </div>
+                 @endif
+                 <!-- ici un changement -->
+                 <form action="{{ route('posts.update', $post->id) }}" method="POST" class="mx-auto">
+                     @csrf
+                     @method('PATCH')
+                     <div class="px-4 py-5 bg-white sm:p-6">
+                         <div class="py-2">
+                             <label for="title" class="block text-sm font-medium text-gray-700">Titre</label>
+                             <input type="text" name="title" id="title" value="{{ $post->title }}"
+                                 class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                 value="{{ old('title') }}">
+                             @error('title')
+                                 <span class="text-red-600">{{ $message }}</span>
+                             @enderror
+                         </div>
+                         <div class="py-2">
+                             <label for="content" class="block text-sm font-medium text-gray-700">Contenu</label>
+                             <textarea id="content" name="content" rows="3" value="{{ $post->content }}"
+                                 class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md">{{ $post->content }}</textarea>
+                             @error('content')
+                                 <span class="text-red-600">{{ $message }}</span>
+                             @enderror
+                         </div>
+                         <div class="py-2">
+                             <input type="submit"
+                                 class="cursor-pointer inline-flex items-center w-1/4 py-4 border border-gray-400 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white justify-center"
+                                 value="Changer">
+                         </div>
+                     </div>
+                 </form>
+             </div>
+         </div>
+     </div>
+ </x-app-layout>
 
 
 
-    <!-- Fin du code -->
-    <!--
-@endsection -->
+ <!-- Fin du code -->
+ <!--
